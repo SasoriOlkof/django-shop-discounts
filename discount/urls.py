10bab6be27c05882fd59fc96fa517740171f3576
SasoriOlkof <sasori@sasori.es>
2017-11-07 12:11:43 +0100

    update to python 3.5, django 1.10 and django-shop  0.10.2

--- discount/urls.py
+++ discount/urls.py
@@ -1,18 +1,13 @@
-try:
-    # django 1.6+
-    from django.conf.urls import patterns,url
-except ImportError:
-    # django <1.6
-    from django.conf.urls.defaults import patterns,url
+
+from django.conf.urls import url
+
+from .views import CartDiscountCodeDeleteView, CartDiscountCodeCreateView
 
 
-from discount.views import CartDiscountCodeDeleteView, CartDiscountCodeCreateView
-
-
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^discount_code/$', CartDiscountCodeCreateView.as_view(), 
         name='discount_cartdiscountcode_create'),
     url(r'^discount_code/delete/$', CartDiscountCodeDeleteView.as_view(), 
         name='discount_cartdiscountcode_delete'),
-    )
+    ]
 
